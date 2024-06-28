7a00759381f09a6b53c7f95eb4e58e60faffca6d
realkoder <alex8970@stud.kea.dk>
2024-06-29 00:25:49 +0200

    Fixed error and setup for view

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,14 @@
 "use client";
 
-import DialogBox from "@/components/DialogBox/DialogBox";
 import { Toaster } from "sonner";
 
 export default function Home() {
-  
-
   return (
     <main className="">
       <div className="flex max-sm:w-80 flex-col text-center items-center">
-        <h1 className="text-lg my-4">
-          TILLYKKE MED DIN FØDSELSDAG BIRTHDAY BOI MADS
-        </h1>
+        <h1 className="text-lg my-4">TILLYKKE MED DAGEN MARI</h1>        
         <Toaster />
-        <h2 className="text-lg my-4">{textContent}</h2>
-      </div>     
+      </div>
     </main>
   );
 }
