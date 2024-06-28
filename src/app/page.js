8cea5eb69e631f8b2a6592d27aaa784f9abe05b0
realkoder <alex8970@stud.kea.dk>
2024-06-29 00:56:01 +0200

    Fixed the background

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,20 @@
 "use client";
 
+import Image from "next/image";
 import { Toaster } from "sonner";
 
 export default function Home() {
   return (
-    <main className="">
-      <div className="flex max-sm:w-80 flex-col text-center items-center">
-        <h1 className="text-lg my-4">TILLYKKE MED DAGEN MARI</h1>        
-        <Toaster />
-      </div>
+    <main className="text-center">
+      <Toaster />
+      <h1 className="text-lg my-4">TILLYKKE MED DAGEN MARI</h1>
+
+      <Image
+        src="https://i.etsystatic.com/24605473/r/il/3cebc7/5187952890/il_1588xN.5187952890_8cen.jpg"
+        alt="Grand danois wearing costume"
+        width={800}
+        height={800}
+      />
     </main>
   );
 }
